0ec762c83bc0abee78bcd0a9027d899cb4019078
ChedlyRebai <chedly.rebai123@gmail.com>
2025-11-04 08:48:57 +0100

    Remove unused import in CallsRestController and create AgentController class

--- src/main/java/tn/esprit/chedlyrebai/Controllers/CallsRestController.java
+++ src/main/java/tn/esprit/chedlyrebai/Controllers/CallsRestController.java
@@ -5,13 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tn.esprit.chedlyrebai.domain.Calls;
 import tn.esprit.chedlyrebai.repositories.CallsRepositories;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 
 @RestController
